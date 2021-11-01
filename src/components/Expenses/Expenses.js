8fdcef6843c18e1b58730251acb6674ec5277858
Anshuman_Bhowmick <anshumanbhowmick2@gmail.com>
2021-11-01 16:15:00 +0530

    adding no expense error message

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -8,18 +8,25 @@ const Expenses = (props) => {
   const yearsubmitHandler = (selectedYear) => {
     selectFiltered(selectedYear);
   };
+  const filteredYear = props.item.filter((expense) => {
+    return expense.date.getFullYear().toString() === filtered;
+  });
   return (
     <div>
       <Card className="expense">
         <ExpenseFilter filter={filtered} onChangeFilter={yearsubmitHandler} />
-        {props.item.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+        {filteredYear.length === 0 ? (
+          <p> No Expenses Found </p>
+        ) : (
+          filteredYear.map((expense) => (
+            <ExpenseItem
+              key={expense.id}
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />
+          ))
+        )}
       </Card>
     </div>
   );
